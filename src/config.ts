65e4510e38b88d492373b48ad4f003f9c07bc14c
abc167661 <108221120+abc167661@users.noreply.github.com>
2026-02-03 18:59:28 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,7 @@ export const siteConfig: SiteConfig = {
 	title: "水木",
 	subtitle: "一个示例网站",
 	siteURL: "https://blog1.xn--lzs769c.com/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteStartDate: "2020-05-04", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
 
